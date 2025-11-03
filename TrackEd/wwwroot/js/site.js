efe2fc9ec968af8d4456212cf14ac6017882c22e
Will Paxton <43458264+willpaxton@users.noreply.github.com>
2025-11-03 14:55:14 -0500

    stuck on how to implement javascript in this project, will pick back up later

--- TrackEd/wwwroot/js/site.js
+++ TrackEd/wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0,
+};
+
+function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+function Updatelocation() {
+    getCurrentPosition(success, error, options)
+}
+
+function success(pos) {
+    var crd = pos.coords;
+    var long = crd.longitude;
+    var lat = crd.latitude;
+    var source="https://www.google.com/maps/embed/v1/view?key=AIzaSyBpSX02zBnR-ueioYxcK67SF3DIpyo4hWE&center=" + long + "," + lat +"&zoom=18"
+    document.getElementById("map").src = source;
+}
+
